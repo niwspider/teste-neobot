47917cec0532df5ca5e7ea6772cbf0bbb7dbd4e8
niwspider <140329373+niwspider@users.noreply.github.com>
2025-05-21 10:06:16 -0300

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,23 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
-    // Carrega o CSS do chatbot
-    const style = document.createElement("link");
-    style.rel = "stylesheet";
-    style.href = "https://neobot-ecru.vercel.app/style.css";
-    document.head.appendChild(style);
-  
-    // Carrega o HTML do chatbot
-    fetch("https://neobot-ecru.vercel.app/index.html")
-      .then(res => res.text())
-      .then(html => {
-        const container = document.createElement("div");
-        container.innerHTML = html;
-        document.body.appendChild(container);
-  
-        // Depois que o HTML for inserido, carregamos o script
-        const script = document.createElement("script");
-        script.src = "https://neobot-ecru.vercel.app/script.js";
-        script.defer = true;
-        document.body.appendChild(script);
-      });
-  });
-  
+  // Carrega o CSS do chatbot
+  const style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.href = "https://steady-brigadeiros-81da3c.netlify.app/style.css";
+  document.head.appendChild(style);
+
+  // Carrega o HTML do chatbot
+  fetch("https://steady-brigadeiros-81da3c.netlify.app/index.html")
+    .then(res => res.text())
+    .then(html => {
+      const container = document.createElement("div");
+      container.innerHTML = html;
+      document.body.appendChild(container);
+
+      // Depois que o HTML for inserido, carregamos o script principal
+      const script = document.createElement("script");
+      script.src = "https://steady-brigadeiros-81da3c.netlify.app/script.js";
+      script.defer = true;
+      document.body.appendChild(script);
+    });
+});
